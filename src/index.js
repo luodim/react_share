238350a58d2d29f21b81feb008de85825ac09049
luodim <luodim@gmail.com>
2018-10-19 18:48:46 +0800

    function:1.修改标题及图标设置
    Signed-off-by: luodim <luodim@gmail.com>

--- src/index.js
+++ src/index.js
@@ -9,22 +9,30 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 import { createStore, combineReducers} from 'redux'
 import { Provider } from 'react-redux'
 import { rootReducer } from './reducers/IndicatorReducer.js'
+import { Helmet } from "react-helmet";
 import './index.css'
 
 const store = createStore(rootReducer)
 
 const el = (
-    <Provider store={store}>
-	  <Router>
-	    <Switch>
-	      <Route exact path='/' component={Splash}/>
-	      <Route path='/login' component={Login}/>
-          <Route path='/edit' component={InfoEdit}/>
-          <Route path='/home' component={Home}/>
-          <Route path='/detail' component={Detail}/>
-        </Switch>
-      </Router>
-    </Provider>)
+    <div className='application'>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>ShareHub</title>
+        <link rel="shortcut icon" href="/share_hub_logo.png" />
+      </Helmet>
+      <Provider store={store}>
+	      <Router>
+	        <Switch>
+	          <Route exact path='/' component={Splash}/>
+	          <Route path='/login' component={Login}/>
+            <Route path='/edit' component={InfoEdit}/>
+            <Route path='/home' component={Home}/>
+            <Route path='/detail' component={Detail}/>
+          </Switch>
+        </Router>
+      </Provider>
+    </div>)
 
 ReactDOM.render(el, document.getElementById('root'))
 
